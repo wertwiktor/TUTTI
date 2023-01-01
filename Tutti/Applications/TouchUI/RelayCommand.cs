5abc68e584366e28e4f6f23cda8495959a7d171e
wittk <witt.konrad@gmail.com>
2023-01-01 22:14:00 +0100

    Implemented initial version of IdentifictionDeviceService

---
+++ Tutti/Applications/TouchUI/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Input;
+using System;
+
+public class RelayCommand : ICommand
+{
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+    private Action _methodToExecute;
+    private Func<bool> _canExecuteEvaluator;
+    public RelayCommand(Action methodToExecute, Func<bool> canExecuteEvaluator)
+    {
+        _methodToExecute = methodToExecute;
+        _canExecuteEvaluator = canExecuteEvaluator;
+    }
+    public RelayCommand(Action methodToExecute)
+        : this(methodToExecute, null)
+    {
+    }
+    public bool CanExecute(object parameter)
+    {
+        if (_canExecuteEvaluator == null)
+        {
+            return true;
+        }
+        else
+        {
+            bool result = _canExecuteEvaluator.Invoke();
+            return result;
+        }
+    }
+    public void Execute(object parameter)
+    {
+        _methodToExecute.Invoke();
+    }
+}
---
+++ Tutti/Services/IdentificationDeviceService/DataContracts/IdentificationOccuredEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.IdentificationDeviceService.DataContracts
+{
+    public class IdentificationOccuredEventArgs : EventArgs
+    {
+        public IdentificationOccuredEventArgs() { }
+        public IdentificationOccuredEventArgs(string identifier) { Identifier = identifier; }
+
+        public string Identifier { get; set; }
+
+    }
+}
