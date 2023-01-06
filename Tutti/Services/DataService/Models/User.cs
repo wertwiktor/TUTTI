cdc6226cbfe8b8cf5eb378e11fc630b035b5c76d
wittk <witt.konrad@gmail.com>
2023-01-06 22:11:01 +0100

    User data model extended.

--- Tutti/Services/DataService/Models/User.cs
+++ Tutti/Services/DataService/Models/User.cs
@@ -4,7 +4,12 @@
     {
         public long Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; } = string.Empty;
         public string Identifier { get; set; } = string.Empty;
+        public int Level { get; set; }
+        public string Nationality { get; set; }
         public List<TimeStamp> TimeStamps { get; set; }
     }
 }
