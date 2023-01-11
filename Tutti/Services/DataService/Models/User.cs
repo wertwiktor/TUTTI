c3d8f4b5c90d23fba003bd87bbe42ace12c31a0f
wittk <witt.konrad@gmail.com>
2023-01-11 21:30:01 +0100

    Added migration for removing nulls from database.

--- Tutti/Services/DataService/Models/User.cs
+++ Tutti/Services/DataService/Models/User.cs
@@ -1,16 +1,34 @@
-﻿namespace DataService.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+
+namespace DataService.Models
 {
     public class User
     {
         public long Id { get; set; }
+        
+        [Required(AllowEmptyStrings = true)]
         public string Name { get; set; } = string.Empty;
+        
+        [Required(AllowEmptyStrings = true)]
         public string Surname { get; set; } = string.Empty;
-        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateOfBirth { get; set; } = (DateTime)SqlDateTime.MinValue;
+        
+        [Required(AllowEmptyStrings = true)]
         public string Email { get; set; } = string.Empty;
+        
+        [Required(AllowEmptyStrings = true)]
         public string Identifier { get; set; } = string.Empty;
+        
         public UserLevel Level { get; set; }
-        public string Nationality { get; set; }
-        public string PhoneNumber { get; set; }
+        
+        [Required(AllowEmptyStrings = true)]
+        public string Nationality { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = true)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
         public List<TimeStamp> TimeStamps { get; set; }
     }
 }
