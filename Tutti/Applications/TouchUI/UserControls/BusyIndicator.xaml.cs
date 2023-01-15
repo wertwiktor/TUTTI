fcb54a8e617a24858b22e55ff4bc61cfde187a1e
wittk <witt.konrad@gmail.com>
2023-01-15 11:56:27 +0100

    Initial implementation of busy indicator.

---
+++ Tutti/Applications/TouchUI/UserControls/BusyIndicator.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TouchUI.UserControls
+{
+    /// <summary>
+    /// Interaction logic for BusyIndicator.xaml
+    /// </summary>
+    public partial class BusyIndicator : UserControl
+    {
+
+        public BusyIndicator()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty BackgroundProperty = DependencyProperty.Register(
+            "Background", typeof(Brush), typeof(BusyIndicator));
+
+        public static readonly DependencyProperty IsBusyProperty = DependencyProperty.Register(
+            "IsBusy", typeof(bool), typeof(BusyIndicator));
+
+        public static readonly DependencyProperty BusyMessageProperty = DependencyProperty.Register(
+            "BusyMessage", typeof(string), typeof(BusyIndicator));
+
+        public new Brush Background
+        {
+            get
+            {
+                return (Brush)GetValue(BackgroundProperty);
+            }
+            set
+            {
+                SetValue(BackgroundProperty, value);
+            }
+        }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return (bool)GetValue(IsBusyProperty);
+            }
+            set
+            {
+                SetValue(IsBusyProperty, value);
+            }
+        }
+
+        public string BusyMessage
+        {
+            get
+            {
+                return (string)GetValue(BusyMessageProperty);
+            }
+            set
+            {
+                SetValue(BusyMessageProperty, value);
+            }
+        }
+    }
+}
