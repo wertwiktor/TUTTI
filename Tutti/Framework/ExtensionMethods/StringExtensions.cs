b4b3eb3ca40e843e0a1a8485bb9d697473abef13
wittk <witt.konrad@gmail.com>
2023-01-15 07:52:01 +0100

    Created Framework project. Added string extension method Here().

---
+++ Tutti/Framework/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string Here<T>(this T message, [CallerFilePath] string sourceFile = "", [CallerMemberName] string sourceMethod = "", [CallerLineNumber] int sourceLine = 0)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(sourceFile);
+            return $"[{fileName}.{sourceMethod}:{sourceLine}] {message}";
+        }
+    }
+}
