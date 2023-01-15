96a9d093cd736585733538a43f5a91b2224b19f2
wittk <witt.konrad@gmail.com>
2023-01-15 08:11:45 +0100

    Moved clock backend to MainViewModel.

--- Tutti/Applications/TouchUI/ViewModels/MainViewModel.cs
+++ Tutti/Applications/TouchUI/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Media3D;
+using System.Windows.Threading;
 using TouchUI.Services.Navigation;
 
 namespace TouchUI.ViewModels
@@ -14,15 +15,35 @@ namespace TouchUI.ViewModels
         private readonly INavigationService _navigationService;
 
         private NavigationViewModelBase _currentViewModel;
+        private DateTime _clockDateTime = DateTime.Now;
+        private DispatcherTimer _clockDisplayTimer = new DispatcherTimer();
         public MainViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
             _navigationService.NavigationChanged += OnNavigationServiceNavigationChanged;
+            InitializeClockDisplayTimer();
         }
 
         private void OnNavigationServiceNavigationChanged(NavigationViewModelBase navigatedViewModel)
         {
             CurrentViewModel= navigatedViewModel;
+        }
+
+        private void InitializeClockDisplayTimer()
+        {
+            _clockDisplayTimer.Interval = TimeSpan.FromSeconds(1);
+            _clockDisplayTimer.Tick += OnClockDisplayTimerElapsed;
+            _clockDisplayTimer.Start();
+        }
+
+        private void UninitializeClockDisplayTimer()
+        {
+            _clockDisplayTimer.Stop();
+        }
+
+        private void OnClockDisplayTimerElapsed(object? sender, EventArgs e)
+        {
+            ClockDateTime = DateTime.Now;
         }
 
         public NavigationViewModelBase CurrentViewModel
@@ -35,6 +56,19 @@ namespace TouchUI.ViewModels
             {
                 _currentViewModel?.Uninitialize();
                 _currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime ClockDateTime
+        {
+            get
+            {
+                return _clockDateTime;
+            }
+            set
+            {
+                _clockDateTime = value;
                 OnPropertyChanged();
             }
         }
